969b0c2cf46c342015bde09bfd459896c8be85ac
vhalbert <vhalbert@jboss.org>
2016-12-16 08:22:37 -0500

    updates

--- install-ext-mat-jdg.sh
+++ install-ext-mat-jdg.sh
@@ -1,3 +1,14 @@
 export PATH=binaries/apache-ant-1.9.7/bin:$PATH
 export ANT_HOME=binaries/apache-ant-1.9.7
+
+if [ "x$1" = "x" ]; then
+    echo "options to run  ./install-ext-mat-jdg.sh {option}"
+    echo "where option is:"
+    echo "   setup-jdg"
+    echo "   start-jdg-server"
+    echo "   setup-jdv or setup-usecase"
+    echo "   start-jdv-server"
+    exit
+fi
+
 ant $1 -propertyfile ./jdv-ext-mat-jdg/build.properties

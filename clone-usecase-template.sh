7c67d9185dbd3297069dc8dedd70316461ab448e
vhalbert <vhalbert@jboss.org>
2017-01-13 07:56:14 -0500

    update cloning

--- clone-usecase-template.sh
+++ clone-usecase-template.sh
@@ -1,9 +1,11 @@
 export PATH=binaries/apache-ant-1.9.7/bin:$PATH
 export ANT_HOME=binaries/apache-ant-1.9.7
 
-#  script used to clone the template usecase
+#  script used to setup a use case by cloning the template usecase 
 
 if [ "x$1" = "x" ]; then
+    echo "This script is used to setup a new use case by cloning the template usecase"
+    echo "  "
     echo "options to run  ./clone-usecase-template.sh {usecase}"
     echo "where usecase is name of the usecase to create"
     echo "      and will create a corresponding directory"

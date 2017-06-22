0a8323935a49f6c5e5023a92e1c017559d0980aa
vhalbert <vhalbert@jboss.org>
2017-06-22 10:57:52 -0400

    JDV 6.4 changes

--- clone-usecase-template.sh
+++ clone-usecase-template.sh
@@ -4,15 +4,23 @@ export ANT_HOME=binaries/apache-ant-1.9.7
 #  script used to setup a use case by cloning the template usecase 
 
 if [ "x$1" = "x" ]; then
-    echo "This script is used to setup a new use case by cloning the template usecase"
+    echo "This script is used to setup a new use case by cloning an existing usecase"
     echo "  "
-    echo "options to run  ./clone-usecase-template.sh {usecase}"
-    echo "where usecase is name of the usecase to create"
-    echo "      and will create a corresponding directory"
+    echo "options to run  ./clone-usecase-template.sh {new usecase} [existing usecase]"
+    echo "where new usecase is name of the usecase to create"
+    echo "                    and will create a corresponding directory"
+    echo "      existing usecase is the one to copy, if left blank, usecase_template will be copied"	
 
     exit
 fi
 
+TEMPLATE=usecase_template
 
-ant clone-template -Dnew-usecase=$1  -Dqs-example=usecase_template
+if [ ! "x$2" = "x" ]; then
+	TEMPLATE=$2    
+fi
+
+echo "Cloning usecase: $TEMPLATE to create $1"
+
+ant clone-template -Dnew-usecase=$1  -Dqs-example=$TEMPLATE
 
